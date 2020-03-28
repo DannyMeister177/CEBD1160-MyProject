3e7a8704f76640b2331ef2a98264c02b7030adb8
DannyMeister177 <danny.goyer177@gmail.com>
2020-03-28 18:55:02 -0400

    final changes (hopefully)

--- Employee-Flight-Risk_Analysis.py
+++ Employee-Flight-Risk_Analysis.py
@@ -1,9 +1,195 @@
 # imports
 import pandas as pd
+import os
+import seaborn as sns
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, fbeta_score, recall_score, plot_confusion_matrix
+from sklearn.metrics import plot_precision_recall_curve
+import matplotlib.pyplot as plt
+
+sns.set(style='darkgrid')
 
 # Load the data
 df = pd.read_csv('data/WA_Fn-UseC_-HR-Employee-Attrition.csv')
 # drop irrelevant columns
-df.drop(['EmployeeNumber', 'EmployeeCount', 'StandardHours', 'Over18'], axis=1, inplace=True)
+df.drop(['EmployeeNumber', 'EmployeeCount', 'StandardHours', 'Over18', 'JobLevel'],
+        axis=1, inplace=True)
+
+# ## Pre-processing
+#
+#
+# At a high level, this involves:
+# 1. Encoding Target column with pandas.get_dummies()
+# 2. One-hot encoding the nominal features using sklearn.OneHotEncoder
+# 3. Scale all features to lie between $[0, 1]$
+# 4. After running a GridSearchCV with RandomForestClassifer (not included here),
+#    drop 17 lowest-ranked features.
+
+# #### 1. Dropping numerical columns used for plotting
+Attrition_enc = pd.get_dummies(df['Attrition'], drop_first=True)
+Attrition_enc.columns = ['Attrition-enc']
+
+# #### 3. One-hot encoding the nominal features using sklearn.OneHotEncoder
+
+# First separate the target and feature matrix
+X = df.drop('Attrition', axis='columns')
+y = Attrition_enc['Attrition-enc']
+
+# Now apply OneHotEncoder to the nominal columns using ColumnTransformer
+
+# get nominal categorical columns in list
+cat_cols = X.columns[X.dtypes == object].tolist()
+
+# set up the ColumnTransformer and OneHotEncoder
+ct = ColumnTransformer(
+    [('one_hot_encoder', OneHotEncoder(sparse=False, drop='first'), cat_cols)],
+    remainder='passthrough'
+)
+
+# set up column names for dummy columns which will be used when we create a new X dataframe
+dummies = ['BusinessTravel_Travel_Frequently',
+           'BusinessTravel_Travel_Rarely',
+           'Department_Research & Development',
+           'Department_Sales',
+           'EducationField_Life Sciences',
+           'EducationField_Marketing',
+           'EducationField_Medical',
+           'EducationField_Other',
+           'EducationField_Technical Degree',
+           'Gender_Female',
+           'JobRole_Human Resources',
+           'JobRole_Laboratory Technician',
+           'JobRole_Manager',
+           'JobRole_Manufacturing Director',
+           'JobRole_Research Director',
+           'JobRole_Research Scientist',
+           'JobRole_Sales Executive',
+           'JobRole_Sales Representative',
+           'MaritalStatus_Married',
+           'MaritalStatus_Single',
+           'OverTime_Yes']
+
+# Now let's get the full column list by appending dummies to the original columns list of X minus
+# the categorical columns themselves (since we are going to use one-hot-encoded columns instead)
+new_cols = dummies + list(X.columns)
+new_cols = [e for e in new_cols if e not in cat_cols]
+
+# now we apply the ColumnTransformer to X and create a new dataframe
+X = pd.DataFrame(ct.fit_transform(X), columns=new_cols)
+
+# #### 4. Scale all features to lie between \[0, 1]
+
+# We know the dummy features are nominal and are either 0 or 1 already so we do not need to
+# normalize them. However, all other features including the ordinal categorical variables will be
+# scaled to lie in the interval $[0, 1]$
+
+# Get all non-dummy columns
+cols_non_dummy = [col for col in list(X.columns) if col not in dummies]
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaler.fit(X[cols_non_dummy])
+scaled_arr = scaler.transform(X[cols_non_dummy])
+X_scaled = pd.DataFrame(scaled_arr, columns=cols_non_dummy)
+
+# Now get the rest of the X dataframe appended
+X_scaled = pd.concat([X.drop(cols_non_dummy, axis=1), X_scaled], axis=1)
+
+# 4. After running a GridSearchCV with RandomForestClassifer (not included here),
+#    drop 17 lowest-ranked features. I didn't include the GridSearch as it was very long to run.
+
+cols2drop = ['WorkLifeBalance',
+             'MaritalStatus_Married',
+             'JobRole_Laboratory Technician',
+             'Education',
+             'EducationField_Technical Degree',
+             'EducationField_Medical',
+             'EducationField_Life Sciences',
+             'BusinessTravel_Travel_Rarely',
+             'JobRole_Research Scientist',
+             'BusinessTravel_Non-Travel',
+             'JobRole_Manufacturing Director',
+             'EducationField_Marketing',
+             'PerformanceRating',
+             'JobRole_Manager',
+             'EducationField_Other',
+             'JobRole_Human Resources',
+             'JobRole_Research Director']
+
+# Set up cols to keep
+cols2keep = [i for i in X_scaled.columns if i not in cols2drop]
+
+# drop from dataset
+X_red = X_scaled[cols2keep]  # this will leave 27 columns in X_Red
+
+### Model with RandomForestClassifier
+
+# test-train split
+X_train, X_test, y_train, y_test = train_test_split(X_red, y, test_size=0.25, random_state=666)
+
+# parameters to use (found with GridSearchCV - not included in script due to long run-time)
+rf_params = {'class_weight': {0: 0.12, 1: 0.88}, 'max_depth': 10,
+             'max_features': 0.15, 'min_samples_leaf': 20,
+             'min_samples_split': 10, 'min_weight_fraction_leaf': 0.001,
+             'n_estimators': 120, 'n_jobs': -1, 'random_state': 666}
+
+# train and fit
+rf = RandomForestClassifier(**rf_params)  # use dict unpacking
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+
+# string to print
+buffer_str = '\n'
+
+# get score results
+fbeta = fbeta_score(y_test, y_pred, beta=1.75)
+recall = recall_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+# now append the actual results to the output string
+buffer_str += f'F-beta: {fbeta:0.3f}\n'
+buffer_str += '-' * 40 + '\n'
+buffer_str += f'recall: {recall:0.3f}\n'
+buffer_str += '-' * 40 + '\n\n'
+buffer_str += f'Classification Report:\n{report}\n\n'
+buffer_str += '=' * 80 + '\n\n'
+
+# print
+print(buffer_str)
+
+### Prepare performance plots
+
+# confusion matrix
+class_names = sorted(df['Attrition'].unique())
+np.set_printoptions(precision=3)
+disp = plot_confusion_matrix(rf, X_test, y_test,
+                             display_labels=class_names,
+                             cmap=plt.cm.Greens,
+                             normalize=None,
+                             values_format='.3g')
+disp.ax_.set_title("Confusion matrix")
+plt.show()
+
+# print confusion matrix
+print("Confusion matrix")
+print(disp.confusion_matrix)
+
+# Save to file
+os.makedirs('plots', exist_ok=True)
+plt.savefig('plots/ConfusionMatrix.png')
+
+# Precision-Recall Curve
+# noinspection PyTypeChecker
+disp = plot_precision_recall_curve(rf, X_test, y_test, color='g')
+x_data = disp.line_.get_xdata()
+y_data = disp.line_.get_ydata()
+disp.ax_.fill_between(x_data, y_data, alpha=0.25, color='g')
+disp.ax_.set_title(f'Precision-Recall curve')
+plt.show()
+
+# Save to file
+os.makedirs('plots', exist_ok=True)
+plt.savefig('plots/Precision-Recall_curve.png')
--- README.md
+++ README.md
@@ -2,17 +2,16 @@
 
 | Name | Date |
 |:-------|:---------------|
-|Daniel Goyer|TBD|
+|Daniel Goyer|March 28, 2020|
 
 -----
 
 ### Resources
 Your repository should include the following:
 
-- Python script for your analysis: `tbd.py`
-- Results figure/saved file: `tbd.py`
+- Python script for your analysis: `Employee-Flight-Risk_Analyis.py`
+- Results figure/saved file: `plots/`
 - runtime-instructions in a file named RUNME.md
-
 -----
 
 ## Research Question
@@ -20,13 +19,6 @@ Your repository should include the following:
 <b>What are the most important factors predicting employee flight (attrition)?</b>
 
 ### Abstract
-
-4 sentence longer explanation about your research question. Include:
-
-- opportunity (what data do we have)
-- challenge (what is the "problem" we could solve with this dataset)
-- action (how will we try to solve this problem/answer this question)
-- resolution (what did we end up producing)
 
 Predicting employee flight (also known as Attrition or Churn) is a principal concern of Human 
 Resource departments as it is typically very costly to replace employees. According to 
@@ -36,7 +28,9 @@ the average cost to replace an employee is <i>"20 percent of annual salary for m
 $8,000."</i> according to one study. Using the dataset which has fictional employee data along with 
 attrition I attempted to determine which factors can help predict employee flight. I found it very 
 challenging as the data is significantly imbalanced (only 16% of employees left) and also has quite
-a few features (49 columns once processed). 
+a few features (49 columns once processed). The model produced was able to provide which factors are
+ most likely to predict attrition but the performance was not up to the challenge but has potential 
+ if more sophisticated methods are used to treat the imbalance.
 
 ### Introduction
 
@@ -48,27 +42,65 @@ the employee left the company or not.
 
 ### Methods
 
-Brief (no more than 1-2 paragraph) description about how you decided to approach solving it. 
-Include:
+After exploring some other algorithms, I decided to use scikit-learn RandomForestClassifer for two
+reasons, 1) it provided a way to choose features that were important in prediction, and 2) it had a 
+method of predicting not only the outcome itself (attrition or no attrition) but also the 
+probability which is more useful as a model for HR departments to monitor risk dynamically.
 
-- pseudocode for this method (either created by you or cited from somewhere else)
-- why you chose this method
+The scikit-learn documentation on the Random Forest algorithm can be found
+[here](https://scikit-learn.org/stable/modules/ensemble.html#random-forests).
+Basically, Random Forests create many (in this case 125) Decision Trees by a method called 
+*bagging*, which involves taking the original training dataset of size n and randomly sampling 
+some part of the data of size n' *with replacement* and then repeating this m times creating 
+m different train sets. This has the effect of reducing the variance associated with any one 
+Decision Tree and consequently avoids over-fitting. An additional source of randomness involves 
+selecting only a random subset of features when creating any one decision node of each Decision Tree
+ which is an example of a broader class of techniques called **ensemble learning**.
 
 ### Results
+I chose the metric of F-Beta with Beta = 1.75 as I wanted to weigh recall as more important than 
+precision or any other metric since in an HR context, the cost of a false negative is much higher 
+than a false positive.
 
-Brief (2 paragraph) description about your results. Include:
+Applying the model to the test set, it achieved F-Beta = 0.616 and Recall = 0.754 (of all employees 
+who did leave in the test set, the model predicted 75.4% correctly). The lower F-Beta is a 
+reflection of precision = 0.39. We can see this in the confusion matrix below where of 95 true 
+positives, the model incorrectly predicted 59 of them to be negatives.
 
-- At least 1 figure
-- At least 1 "value" that summarizes either your data or the "performance" of your method
-- A short explanation of both of the above
+![Confusion Matrix](./plots/ConfusionMatrix.png)
+
+Another way to visualize the performance is looking at the trade off in Recall vs Precision:
+
+![Precision-Recall_curve](./plots/Precision-Recall_curve.png)
+
+We can see average precision is only 50% and the curve is quite low.
+
+That being said, the model did provide which features are most important in predicting attrition 
+with OverTime, MonthlyIncome and TotalWorkingYears being the most important.
+
+![Feature Importance](./plots/RF_FeatureRank.png)
 
 ### Discussion
-Brief (no more than 1-2 paragraph) description about what you did. Include:
+The model performed somewhat poorly as there is a low precision. This is due to a couple things:
 
-- interpretation of whether your method "solved" the problem
-- suggested next step that could make it better.
+1. **High Imbalance** - Although I set the weights to be balanced when training the model, it still 
+seems the imbalance is reducing performance. We may be able to get better results by treating the 
+imbalance with some more advanced techniques like Under-sampling or Over-sampling, and trying 
+different Penalization approaches. *(source=5 below)*
+2. **Large number of features** - I did attempt to reduce the features using the random forest 
+feature importances but it did not seem to impact performance too much. More advanced techniques 
+for dimensionality reduction include PCA, ICA, Backward and Forward Elimination, and more. 
+*(source=4 below)*
 
 ### References
-All of the links
+[1. Kaggle dataset](https://www.kaggle.com/pavansubhasht/ibm-hr-analytics-attrition-dataset)
+
+[2. scikit-learn RandomForestClassifer User Guide](https://scikit-learn.org/stable/modules/ensemble.html#random-forests)
+
+[3. Wikipedia Artical on Random Forest Bagging](https://en.wikipedia.org/wiki/Random_forest#Bagging)
+
+[4. Article on Dimensionality Reduction](https://www.analyticsvidhya.com/blog/2018/08/dimensionality-reduction-techniques-python/)
+
+[5. Article on Imbalanced Classes](https://machinelearningmastery.com/tactics-to-combat-imbalanced-classes-in-your-machine-learning-dataset/)
 
 -------
